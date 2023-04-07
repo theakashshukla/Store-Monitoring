76a109fc13560ae64dc3adf80b9929b6abdc4633
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-04-07 20:05:57 +0530

    rds update

--- rds.js
+++ rds.js
@@ -2,6 +2,6 @@ const AWS = require('aws-sdk');
 
 const rds = new AWS.RDS({
   region: 'us-east-1', // Replace with your RDS region
-  accessKeyId: 'your_access_key_id',
-  secretAccessKey: 'your_secret_access_key',
+  accessKeyId: 'AKIARKUMQRU7LRG33NE4',
+  secretAccessKey: 'hMItUj8gghvwSAF6FVgySJRvZXU1R+AoTXBI2kwa',
 });
